5eac1e85912e01ca6300715f7d61a9976a222418
jashiraj <100071696+jashiraj@users.noreply.github.com>
2023-05-19 01:15:40 +0530

    added files

---
+++ src/main/java/in/freshnews/getnews/GetnewsApplication.java
@@ -0,0 +1,13 @@
+package in.freshnews.getnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/freshnews/getnews/GetnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.freshnews.getnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
