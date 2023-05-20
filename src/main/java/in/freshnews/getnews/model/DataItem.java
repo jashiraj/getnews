7dbb55b045eaa77be35dd0d7570d5d07ff87d1fe
vishalksahu <vishal2u_28@yahoo.co.in>
2023-05-20 18:31:34 +0530

    task 11 to show only title,date and time

--- src/main/java/in/freshnews/getnews/model/DataItem.java
+++ src/main/java/in/freshnews/getnews/model/DataItem.java
@@ -3,14 +3,15 @@ package in.freshnews.getnews.model;
 
 @lombok.Data
 public class DataItem {
-  String author;
+  //String author;
   /*    String content;
-    String date;
+
     String id;
     String imageUrl;
     String readMoreURL;
     */
     String title;
+  String date;
   String time;
   //  String url;
 
