c966274a36694966fa0eea79ab00390feb53fe45
vishalksahu <vishal2u_28@yahoo.co.in>
2023-05-25 01:16:00 +0530

    task 13 implementation of hystrix

--- src/main/java/in/freshnews/getnews/GetnewsApplication.java
+++ src/main/java/in/freshnews/getnews/GetnewsApplication.java
@@ -3,8 +3,12 @@ package in.freshnews.getnews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 
 @SpringBootApplication
+@EnableHystrix
+@EnableAutoConfiguration
 public class GetnewsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/in/freshnews/getnews/services/InShortsServiceGateway.java
+++ src/main/java/in/freshnews/getnews/services/InShortsServiceGateway.java
@@ -1,19 +1,22 @@
 package in.freshnews.getnews.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import in.freshnews.getnews.model.Models;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 @Log4j2
-@Component
+@Service
 public class InShortsServiceGateway {
 
     RestTemplate restTemplate = new RestTemplate();
     @Value("${backend.inshorts.url}")
     private String resourceUrl;
    // String resourceUrl= "https://inshorts.deta.dev/news?category=";
+    @HystrixCommand(commandKey = "inshorts", threadPoolKey = "news")
     public Models getNewsByCategory(String category)
     {
         log.info("calling inshorts backend with category- " + category);
@@ -24,4 +27,9 @@ public class InShortsServiceGateway {
         return response.getBody();
     }
 
+/*    private Models defaultGreeting(String username) {
+        Models fallback = new Models();
+        fallback.setSuccess(false);
+        return fallback;
+    }*/
 }
